833635ea3c662ea72e80fa9aa29da3a3b5b78954
Cong Dao <quantran.cv@gmail.com>
2024-06-05 07:43:36 +0000

    Update

--- static/js/leaderboard.js
+++ static/js/leaderboard.js
@@ -3,9 +3,9 @@ var data = [
     { "COUNTRY":"US", "LoC":"New York", "BALANCE":"43,568", "DATE":"18/05/2018" },
     { "COUNTRY":"PL", "LoC":"Kraków", "BALANCE":"12,362", "DATE":"22/06/2018" },
     { "COUNTRY":"AU", "LoC":"Townsville", "BALANCE":"7,569", "DATE":"1/07/2018" },
-    { "COUNTRY":" ", "LoC":"Test:", "BALANCE":"142,072", "DATE":"Test " }
+    { "COUNTRY":"Test", "LoC":"Test:", "BALANCE":"142,072", "DATE":"Test " }
 ];
-var table = document.getElementById('gable');
+var table = document.getElementById('leaderboard-data');
 data.forEach(function(object) {
     var tr = document.createElement('tr');
     tr.innerHTML = '<td>' + object.COUNTRY + '</td>' +
