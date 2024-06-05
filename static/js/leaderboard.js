3773ff3bf4aa6d9a36dc43c0d39a583b30eafbb5
Cong Dao <quantran.cv@gmail.com>
2024-06-05 07:31:19 +0000

    Update

--- static/js/leaderboard.js
+++ static/js/leaderboard.js
@@ -1,37 +1,16 @@
-//first add an event listener for page load
-document.addEventListener( "DOMContentLoaded", get_json_data, false ); // get_json_data is the function name that will fire on page load
-
-//this function is in the event listener and will execute on page load
-function get_json_data(){
-    // Relative URL of external json file
-    var json_url = 'example.json';
-
-    //Build the XMLHttpRequest (aka AJAX Request)
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() { 
-        if (this.readyState == 4 && this.status == 200) {//when a good response is given do this
-
-            var data = JSON.parse(this.responseText); // convert the response to a json object
-            append_json(data);// pass the json object to the append_json function
-        }
-    }
-    //set the request destination and type
-    xmlhttp.open("POST", json_url, true);
-    //set required headers for the request
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    // send the request
-    xmlhttp.send(); // when the request completes it will execute the code in onreadystatechange section
-}
-
-//this function appends the json data to the table 'gable'
-function append_json(data){
-    var table = document.getElementById('gable');
-    data.forEach(function(object) {
-        var tr = document.createElement('tr');
-        tr.innerHTML = '<td>' + object.COUNTRY + '</td>' +
+var data = [
+    { "COUNTRY":"UK", "LoC":"London", "BALANCE":"78,573", "DATE":"1/06/2018" },
+    { "COUNTRY":"US", "LoC":"New York", "BALANCE":"43,568", "DATE":"18/05/2018" },
+    { "COUNTRY":"PL", "LoC":"Kraków", "BALANCE":"12,362", "DATE":"22/06/2018" },
+    { "COUNTRY":"AU", "LoC":"Townsville", "BALANCE":"7,569", "DATE":"1/07/2018" },
+    { "COUNTRY":" ", "LoC":"Test:", "BALANCE":"142,072", "DATE":"Test " }
+];
+var table = document.getElementById('gable');
+data.forEach(function(object) {
+    var tr = document.createElement('tr');
+    tr.innerHTML = '<td>' + object.COUNTRY + '</td>' +
         '<td>' + object.LoC + '</td>' +
         '<td>' + object.BALANCE + '</td>' +
         '<td>' + object.DATE + '</td>';
-        table.appendChild(tr);
-    });
-}
+    table.appendChild(tr);
+});
